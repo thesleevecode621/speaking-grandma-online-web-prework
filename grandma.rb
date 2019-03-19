5c62bef6f28109c451065078ac79d2a856066c9a
boruch d <dubywider@gmail.com>
2019-03-19 15:02:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,4 @@
 def speak_to_grandma(phrase)
-  if phrase ='I LOVE YOU GRANDMA!'
-    return 'I LOVE YOU TOO PUMPKIN!'
+  if phrase ="I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+    
