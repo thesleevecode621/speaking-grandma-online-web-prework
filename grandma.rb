a8d3cef43d82720d3ebbf4837ec98325075f2aa9
boruch d <dubywider@gmail.com>
2019-03-19 15:11:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,12 @@
-def speak_to_grandma(phrase)
-  if (phrase ="I LOVE YOU GRANDMA!")
+ef speak_to_grandma(phrase)
+  if(phrase=="I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
-    elsif (phrase = "Hi Nana, how are you?")
-    return "HUH?! SPEAK UP, SONNY!"
-    elsif (phrase = "Hi")
-    return"HUH?! SPEAK UP, SONNY!"
-   else
+  elsif (phrase=="#{phrase}".upcase)
+    return "NO, NOT SINCE 1938!"
+  else
     return "HUH?! SPEAK UP, SONNY!"
   end
+end
 
     
     
