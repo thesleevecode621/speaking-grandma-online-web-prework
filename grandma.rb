6bf824f05c10f075d14fdfba34fe34a4715decaf
boruch d <dubywider@gmail.com>
2019-03-19 15:03:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,6 @@
 def speak_to_grandma(phrase)
   if phrase ="I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase = 
+    elsif phrase = "Hi Nana, how are you?"
+    return 
     
